055bb19c6b7e80f1b728ee8bbde8d2d38dbeb1f1
Jonathan Hung <hungj@stanford.edu>
2014-11-11 19:32:14 -0800

    edit .net generation to add the same prefix list of nodes to each .net file
    add random edge sampling to timeframe

--- edges_by_timestamp.py
+++ edges_by_timestamp.py
@@ -1,38 +1,32 @@
 import time
 import re
+import random
 
 def strtotime(strtime):
     return time.strptime(strtime, "%Y-%m-%d %H:%M:%S")
 
 # omits timestamp (since each edge can have multiple timestamps)
 # start and end should be in format "YYYY-MM-DD HH:MM:SS"
-def edges_by_weight(file, start, end):
+def edges_by_weight(file, start, end, perc=1.0):
     filename = "./edges_weight_%d_%d.net" % (int(time.mktime(strtotime(start))), int(time.mktime(strtotime(end))))
     file_by_weight = open(filename, 'w')
     edges = {}
-    nodes = set()
-    external_to_internal = {}
     for line in file:
-        tokens = line.split(',')
-        if strtotime(tokens[0]) >= strtotime(start) and strtotime(tokens[0]) <= strtotime(end):
-            src = int(tokens[1])
-            dst = int(tokens[2])
-            if (src, dst) in edges:
-                edges[(src, dst)] += 1
-            else:
-                edges[(src, dst)] = 1
-                nodes.add(src)
-                nodes.add(dst)
-    file_by_weight.write("* Vertices: %d\n" % len(nodes))
-    nodes = sorted(nodes)
-    count = 0
-    for i in nodes:
-        file_by_weight.write("%d %d\n" % (count, i))
-        external_to_internal[i] = count
-        count += 1
-    file_by_weight.write("* Edges: %d\n" % (len(edges)))
+        if random.random() < perc:
+            tokens = line.split(',')
+            if strtotime(tokens[0]) >= strtotime(start) and strtotime(tokens[0]) <= strtotime(end):
+                src = int(tokens[1])
+                dst = int(tokens[2])
+                if (src, dst) in edges:
+                    edges[(src, dst)] += 1
+                else:
+                    edges[(src, dst)] = 1
+    vertex_file = open('./vertex_prefix.txt', 'r')
+    for line in vertex_file:
+        file_by_weight.write(line)
+    file_by_weight.write("*Edges %d\n" % (len(edges)))
     for edge in edges:
-        file_by_weight.write("%d %d %d\n" % (external_to_internal[edge[0]], external_to_internal[edge[1]], edges[edge]))
+        file_by_weight.write("%d %d %d\n" % (edge[0], edge[1], edges[edge]))
     file_by_weight.close()
         
 # filters edges by time, all with weight 1
@@ -55,6 +49,17 @@ def get_emails(file):
         email = tokens[1]
         id_to_email[id] = email
     return id_to_email
+
+def write_vertices(emailsf):
+    vertexf = open('./vertex_prefix.txt', 'w')
+    nodes = []
+    for line in emailsf:
+        nodes.append(int(line.split(',')[0]))
+    vertexf.write("*Vertices %d\n" % len(nodes))
+    count = 0
+    for i in nodes:
+        vertexf.write("%d %d\n" % (count, i))
+        count += 1
 
 emailsf = open('./email.txt', 'r')
 edgesf = open('./edges.txt', 'r')
