fa883ede75c9c0a5d26bd2bcd394d185fdfefc08
Mark Ulrich <mulrich@stanford.edu>
2014-12-09 01:46:42 -0800

    Now for each window appropriate EdgeAttrs are generated

--- extract_features.py
+++ extract_features.py
@@ -1,6 +1,8 @@
 from subprocess import Popen, call
 import os
 import collections
+from collections import defaultdict
+from edges_by_timestamp import strtotime, TIME_FORMAT
 
 PREDICT_BIN_PATH = './LPmade/netlib/bin/predict'
 # TODO originally 'PropFlow_5', 'IPageRank_D_0.85', 'JPageRank_D_0.85', 'RootedPageRank_D_0.15'
@@ -41,8 +43,71 @@ def genLabelFile(network_file_name, dirName, output_name='/EdgeWeight.txt'):
         for src in xrange(numNodes):
             for dst in xrange(numNodes):
                 out.write('{} {} {}\n'.format(intToExt[src], intToExt[dst], weights[(src, dst)]))
+    return intToExt, numNodes
 
-def write_features(network_file_name, dirName):
+def getEdgesToEmail(window):
+    startTime = strtotime(window[0].strftime(TIME_FORMAT))
+    endTime = strtotime(window[1].strftime(TIME_FORMAT))
+    edgesToEmail = defaultdict(list)
+    with open('edges.txt') as edgesf:
+        for line in edgesf:
+            tokens = line.split(',')
+            date = strtotime(tokens[0])
+            if date >= startTime and date < endTime:
+                src = int(tokens[1])
+                dst = int(tokens[2])
+                email = tokens[3]
+                edgesToEmail[(src, dst)] += [email.strip()]
+    return edgesToEmail
+
+def getEmailToNlp():
+    nFeats = 2
+    emailToNlp = defaultdict(lambda: [0] * nFeats)
+    with open('nlp_features.out') as nlpf:
+        for line in nlpf:
+            tokens = line.split(',')
+            feats = (float(tokens[1]), float(tokens[nFeats]))
+            emailToNlp[tokens[0]] = feats
+    return emailToNlp, nFeats
+
+def getEmailToMc():
+    first = True
+    with open('message_class.labels') as mcf:
+        for line in mcf:
+            tokens = line.split(' ')
+            if first:
+                first = False
+                nFeats = len(tokens) - 1
+                emailToMc = defaultdict(lambda: [0] * nFeats)
+            emailToMc[tokens[0][1:]] = tuple(int(tokens[x]) for x in xrange(1, nFeats + 1))
+    return emailToMc, nFeats
+
+# Generates an edge attribute file where each row has the format
+# src dest NLP1SUM ... NLPNSUM MC1SUM ... MCNSUM NLP1MEAN ... NLPNMEAN MC1MEAN ... MCNMEAN
+def genAttrFiles(dirName, intToExt, numNodes, window):
+    edgesToEmail = getEdgesToEmail(window)
+    emailToNlp, nNlp = getEmailToNlp()
+    emailToMc, nMc = getEmailToMc()
+
+    def summation(data):
+        return sum(dat for dat in data)
+
+    def mean(data):
+        return 0 if len(data) == 0 else summation(data) / len(data)
+
+    ALGS = [summation, mean]
+
+    with open(dirName + 'EdgeAttrs.txt', 'w') as out:
+        for src in xrange(numNodes):
+            for dst in xrange(numNodes):
+                srcExt = intToExt[src]
+                dstExt = intToExt[dst]
+                allData = [[emailToNlp[email][i] for email in edgesToEmail[(srcExt, dstExt)]] for i in range(nNlp)] + \
+                        [[emailToMc[email][i] for email in edgesToEmail[(srcExt, dstExt)]] for i in range(nMc)]
+                featurePoints = [alg(d) for alg in ALGS for d in allData]
+                out.write((('{} ' * (2 + len(featurePoints)))[:-1] + '\n').format(srcExt, dstExt, *featurePoints))
+
+def write_features(network_file_name, dirName, window):
     if not os.path.isfile(network_file_name):
         print('Sorry, {} is not a valid file.'.format(network_file_name))
         return
@@ -52,7 +117,8 @@ def write_features(network_file_name, dirName):
         out = open(dirName + '/' + pred + '.txt', 'w')
         parr.append(Popen([PREDICT_BIN_PATH, '-f', network_file_name, pred], stdout=out))
         outarr.append(out)
-    genLabelFile(network_file_name, dirName)
+    intToExt, numNodes = genLabelFile(network_file_name, dirName)
+    genAttrFiles(dirName, intToExt, numNodes, window)
     for p in parr:
         p.wait()
     for out in outarr:
--- windows.py
+++ windows.py
@@ -5,18 +5,21 @@ from extract_features import write_features, genLabelFile
 from multiprocessing import Pool
 import datetime
 import time
+import sys
+import os
 
-MIN_TIME = '1999-06-01 00:00:00'
-MAX_TIME = '2001-06-01 00:00:00'
+MIN_TIME = '2000-01-01 00:00:00'
+MAX_TIME = '2001-05-01 00:00:00'
 
 def getWindowDateTimes(start_time=MIN_TIME, time_increment=datetime.timedelta(days = 100), final_end_time=MAX_TIME):
     start_time = toDateTime(start_time)
     final_end_time = toDateTime(final_end_time)
     while True:
         end_time = start_time + time_increment
-        if end_time > final_end_time:
+        next_time = end_time + time_increment
+        if next_time > final_end_time:
             break
-        yield (start_time, end_time)
+        yield (start_time, end_time, next_time)
         start_time = end_time
 
 def getWindowEpochs(start_time=MIN_TIME, time_increment=datetime.timedelta(days = 100), final_end_time=MAX_TIME):
@@ -29,17 +32,17 @@ def toDateTime(strtime):
     return datetime.datetime(*(strtotime(strtime)[0:6]))
 
 def generate_window_networks_and_features(windows, windowName):
+    if os.path.isdir('windows/' + windowName):
+        print 'WARNING {} already exists'.format(windowName)
     for start_time, end_time, next_time in windows:
         directory = getWindowDirName(start_time.strftime(TIME_FORMAT), end_time.strftime(TIME_FORMAT), windowName)
         edges_by_weight(start_time.strftime(TIME_FORMAT), end_time.strftime(TIME_FORMAT), directory, 'network')
-        write_features(directory + 'network.txt', directory)
+        write_features(directory + 'network.txt', directory, (start_time, end_time))
         edges_by_weight(end_time.strftime(TIME_FORMAT), next_time.strftime(TIME_FORMAT), directory, 'future')
         genLabelFile(directory + 'future.txt', directory, 'Labels.txt')
 
-if __name__ == '__main__':
-    # windows = getWindowDateTimes(time_increment=datetime.timedelta(days = 60))
-    # generate_window_networks_and_features(windows)
-    base = toDateTime('2000-01-01 00:00:00')
+def gen_delta_windows():
+    base = toDateTime(MIN_TIME)
     N_DELTAS = 9
     delta = [datetime.timedelta(days = 2**i) for i in range(N_DELTAS)]
 
@@ -47,6 +50,23 @@ if __name__ == '__main__':
         i, j = data
         generate_window_networks_and_features([(base - delta[i], base, base + delta[j])], '{} {} (days past, days future)'.format(i, j))
 
-    p = Pool(processes=3)
+    # p = Pool(processes=3)
     for i in range(N_DELTAS):
-        p.map(call_gwnaf, [(i, j) for j in range(N_DELTAS)])
+        [call_gwnaf((i, j)) for j in range(N_DELTAS)]
+        # p.map(call_gwnaf, [(i, j) for j in range(N_DELTAS)])
+
+def gen_linear_windows(days=60):
+    windows = getWindowDateTimes(time_increment=datetime.timedelta(days=days))
+    generate_window_networks_and_features(windows, 'attrdays' + str(days))
+
+if __name__ == '__main__':
+    if sys.argv[1] == 'delta':
+        gen_delta_windows()
+    elif sys.argv[1] == 'linear':
+        gen_linear_windows()
+    elif sys.argv[1] == 'simple':
+        days = 4
+        windows = getWindowDateTimes(start_time='2000-01-01 00:00:00', time_increment=datetime.timedelta(days = days), final_end_time='2000-01-09 00:00:00')
+        generate_window_networks_and_features(windows, 'days{}'.format(days))
+    else:
+        print 'Option not available.'
